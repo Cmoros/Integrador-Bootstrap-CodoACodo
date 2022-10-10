4067ff6eacdeb6a675c54aed8eb1a3648b068b79
Cesar Moros <rdcesar442@gmail.com>
2022-10-09 23:18:55 -0300

    Finished 2nd entry?

--- js/compra.js
+++ js/compra.js
@@ -11,6 +11,8 @@ const inputQuantity = ticketForm.querySelector("#quantity");
 const selectCategory = ticketForm.querySelector("#category");
 const priceSpan = ticketForm.querySelector("#price");
 
+const discountPriceCards = document.querySelectorAll(".card.card-group__card");
+
 function updatePriceSpan() {
   const currentQuantity = inputQuantity.value;
 
@@ -21,9 +23,28 @@ function updatePriceSpan() {
 
   const currentCategory = selectCategory.value;
   const flatPrice = TICKET_PRICE * currentQuantity;
-  priceSpan.innerHTML = Math.ceil(flatPrice * (1 - discounts[currentCategory]));
+  let roundedPrice = Math.ceil(flatPrice * (1 - discounts[currentCategory]));
+  let roundedPriceArray = String(roundedPrice).split("");
+  let price = [];
+  for (let i = 0; i < roundedPriceArray.length; i++) {
+    price.push(roundedPriceArray[i]);
+    if (
+      i + 1 != roundedPriceArray.length &&
+      (roundedPriceArray.length - i - 1) % 3 == 0
+    ) {
+      price.push(".");
+    }
+  }
+  priceSpan.innerHTML = price.join("");
 }
 
 inputQuantity.addEventListener("input", updatePriceSpan);
 selectCategory.addEventListener("input", updatePriceSpan);
-ticketForm.addEventListener('reset', () => priceSpan.innerHTML = "");
+ticketForm.addEventListener("reset", () => (priceSpan.innerHTML = ""));
+
+discountPriceCards.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    selectCategory.value = card.dataset.discount;
+    updatePriceSpan();
+  });
+});
